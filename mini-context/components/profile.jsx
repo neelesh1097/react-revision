3ef1fd48d32e23aa7e8ee957ec55414d4ff1adcc
neele <neelesh6kumar@gmail.com>
2025-11-13 23:56:37 +0530

    mini-context1

---
+++ mini-context/components/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserContext from '../Context/UserContext'
+import {useState , useContext} from 'react'
+
+function profile() {
+  return (
+
+      const {User} = UserContext()
+  )
+}
+
+export default profile
